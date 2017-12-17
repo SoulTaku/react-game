a9fa452cca04df7ec4b3e20bc9b5e5e5012478da
SoulTaku <zombie2912@yahoo.com>
2017-12-17 18:30:31 +0200

    Remove props

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return(
       <div className="container">
-        <Battle place="col-md-4 col-md-offset-4" />
+        <Battle />
       </div>
     );
   }
